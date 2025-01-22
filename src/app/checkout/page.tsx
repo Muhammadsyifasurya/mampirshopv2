d5a57ee562fe1d7a3cdb7e080dfd01ac8d2ff3a2
Muhammad Syifa Surya Saputra <syifamuhammad3139@gmail.com>
2025-01-22 18:11:29 +0700

    styiling button checkout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -63,7 +63,7 @@ const CheckoutPage = () => {
     alert("Order berhasil disimpan!");
   };
   return (
-    <div className="bg-white rounded-lg shadow-lg px-8 py-10 max-w-4xl mx-auto mt-16">
+    <div className="bg-white mt-28 dark:bg-gray-800 rounded-3xl shadow-lg px-8 py-10 max-w-4xl mx-auto">
       {/* Header */}
       <div className="flex items-center justify-between mb-8">
         <div className="flex items-center">
@@ -74,9 +74,11 @@ const CheckoutPage = () => {
             width={48}
             height={48}
           />
-          <div className="text-gray-700 font-bold text-3xl">MampirShop</div>
+          <div className="text-gray-700 dark:text-gray-200 font-bold text-3xl">
+            MampirShop
+          </div>
         </div>
-        <div className="text-gray-600">
+        <div className="text-gray-600 dark:text-gray-400 text-right">
           <div className="font-semibold text-xl mb-2">INVOICE</div>
           <div className="text-sm">Date: {new Date().toLocaleDateString()}</div>
           <div className="text-sm">
@@ -86,42 +88,55 @@ const CheckoutPage = () => {
       </div>
 
       {/* Billing Address */}
-      <div className="border-b-2 border-gray-300 pb-8 mb-8">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">Bill To:</h2>
-        <div className="text-gray-700 mb-2">{user?.name}</div>
-        <div className="text-gray-700 mb-2">
+      <div className="border-b-2 border-gray-300 dark:border-gray-600 pb-8 mb-8">
+        <h2 className="text-2xl font-semibold text-gray-700 dark:text-gray-200 mb-4">
+          Bill To:
+        </h2>
+        <div className="text-gray-700 dark:text-gray-300 mb-2">
+          {user?.name}
+        </div>
+        <div className="text-gray-700 dark:text-gray-300 mb-2">
           Jl. Parangtritis KM 20, Yogyakarta
         </div>
-        <div className="text-gray-700 mb-2">{user?.email}</div>
+        <div className="text-gray-700 dark:text-gray-300 mb-2">
+          {user?.email}
+        </div>
       </div>
 
       {/* Items Table */}
       <table className="w-full text-left mb-8">
         <thead>
           <tr>
-            <th className="text-gray-700 font-semibold uppercase py-3">
+            <th className="text-gray-700 dark:text-gray-300 font-semibold uppercase py-3">
               Description
             </th>
-            <th className="text-gray-700 font-semibold uppercase py-3">
+            <th className="text-gray-700 dark:text-gray-300 font-semibold uppercase py-3">
               Quantity
             </th>
-            <th className="text-gray-700 font-semibold uppercase py-3">
+            <th className="text-gray-700 dark:text-gray-300 font-semibold uppercase py-3">
               Price
             </th>
-            <th className="text-gray-700 font-semibold uppercase py-3">
+            <th className="text-gray-700 dark:text-gray-300 font-semibold uppercase py-3">
               Total
             </th>
           </tr>
         </thead>
         <tbody>
           {invoiceDetails.items.map((product: CartItem) => (
-            <tr key={product.id} className="border-b border-gray-200">
-              <td className="py-4 text-gray-700">{product.title}</td>
-              <td className="py-4 text-gray-700">{product.quantity}</td>
-              <td className="py-4 text-gray-700">
+            <tr
+              key={product.id}
+              className="border-b border-gray-200 dark:border-gray-600"
+            >
+              <td className="py-4 text-gray-700 dark:text-gray-300">
+                {product.title}
+              </td>
+              <td className="py-4 text-gray-700 dark:text-gray-300">
+                {product.quantity}
+              </td>
+              <td className="py-4 text-gray-700 dark:text-gray-300">
                 ${product.price.toFixed(2)}
               </td>
-              <td className="py-4 text-gray-700">
+              <td className="py-4 text-gray-700 dark:text-gray-300">
                 ${(product.price * product.quantity).toFixed(2)}
               </td>
             </tr>
@@ -130,7 +145,7 @@ const CheckoutPage = () => {
       </table>
 
       {/* Subtotal and Discount */}
-      <div className="flex justify-between mb-8 text-gray-700">
+      <div className="flex justify-between mb-8 text-gray-700 dark:text-gray-300">
         <span className="font-medium">Subtotal:</span>
         <span>${invoiceDetails.totalAmount.toFixed(2)}</span>
       </div>
@@ -138,19 +153,21 @@ const CheckoutPage = () => {
       {/* Discount Section */}
       <div className="text-right mb-8">
         <div className="flex flex-col gap-2">
-          <div className="flex justify-between items-center text-gray-700">
+          <div className="flex justify-between items-center text-gray-700 dark:text-gray-300">
             <span className="font-medium">Use code:</span>
-            <span className="text-blue-500 font-semibold">{discountCode}</span>
+            <span className="text-blue-500 dark:text-blue-400 font-semibold">
+              {discountCode}
+            </span>
           </div>
-          <div className="flex justify-between items-center text-gray-700">
+          <div className="flex justify-between items-center text-gray-700 dark:text-gray-300">
             <span className="font-medium">Discount:</span>
-            <span className="text-green-500 font-semibold">
+            <span className="text-green-500 dark:text-green-400 font-semibold">
               {(discountAmount * 100).toFixed(0)}%
             </span>
           </div>
-          <div className="flex justify-between items-center text-gray-700">
+          <div className="flex justify-between items-center text-gray-700 dark:text-gray-300">
             <span className="font-medium">Total Discount:</span>
-            <span className="text-red-500 font-semibold">
+            <span className="text-red-500 dark:text-red-400 font-semibold">
               -${(invoiceDetails.totalAmount * discountAmount).toFixed(2)}
             </span>
           </div>
@@ -158,28 +175,34 @@ const CheckoutPage = () => {
       </div>
 
       {/* Final Total */}
-      <div className="flex justify-between mb-8 text-gray-700 font-bold text-xl">
+      <div className="flex justify-between mb-8 text-gray-700 dark:text-gray-200 font-bold text-xl">
         <span>Total:</span>
         <span>${calculateTotal()}</span>
       </div>
 
       {/* Footer */}
-      <div className="border-t-2 border-gray-300 pt-8">
-        <div className="text-gray-700 mb-2">Thank you for your business!</div>
-        <div className="text-gray-700 mb-2">
-          Payment is due within 30 days. Late payments are subject to fees.
+      <div className="flex justify-between border-t-2 border-gray-300 dark:border-gray-600 pt-8">
+        <div>
+          <div className="text-gray-700 dark:text-gray-300 mb-2">
+            Thank you for your business!
+          </div>
+          <div className="text-gray-700 dark:text-gray-300 mb-2">
+            Payment is due within 30 days. Late payments are subject to fees.
+          </div>
+          <div className="text-gray-700 dark:text-gray-300 mb-2">
+            Payment Method: {invoiceDetails.paymentMethod}
+          </div>
+          <div className="text-blue-700 dark:text-blue-400 underline">
+            syifamuhammad3139@gmail.com
+          </div>
         </div>
-        <div className="text-gray-700 mb-2">
-          Payment Method: {invoiceDetails.paymentMethod}
-        </div>
-        <div className="text-gray-700 mb-2">
-          Please make checks payable to Mampir Shop and mail to:
-        </div>
-        <div className="text-blue-700 underline">
-          syifamuhammad3139@gmail.com
-        </div>
+        <button
+          onClick={handlePayment}
+          className="w-32 mt-16 py-3 text-white bg-gray-800 hover:bg-gray-700 rounded-full shadow-lg transform hover:scale-110 transition-all duration-300 dark:bg-gray-900 dark:hover:bg-gray-800"
+        >
+          Pay Now
+        </button>
       </div>
-      <button onClick={handlePayment}>Pay Now</button>
     </div>
   );
 };
