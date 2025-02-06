a6dc788f8d16172a2b85faf20ceaf894785344b4
Muhammad Syifa Surya Saputra <syifamuhammad3139@gmail.com>
2025-02-06 22:12:42 +0700

    remove : unused syntax

--- src/components/ui/Filter.tsx
+++ src/components/ui/Filter.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useCart } from "@/context/CartContext";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Filter = () => {
   const {
